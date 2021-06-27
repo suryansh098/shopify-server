6f6ac753dcbd4cb7c1ddb1b3176cacf0540027de
Suryansh Yadav <suryanshyadav160@gmail.com>
2021-06-28 00:04:49 +0530

    created delete product route

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -64,9 +64,7 @@ productRouter.put(
   expressAsyncHandler(async (req, res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
-
     if (product) {
-
       product.name = req.body.name;
       product.price = req.body.price;
       product.image = req. body.image;
@@ -74,18 +72,30 @@ productRouter.put(
       product.brand = req.body.brand;
       product.countInStock = req.body.countInStock;
       product.description = req.body.description;
-
       const updatedProduct = await product.save();
       res.send({
         message: 'Product Updated',
         product: updatedProduct
       });
-
     } else {
-
       res.status(404).send({ message: 'Product Not Found' });
     }
   })
-)
+);
+
+productRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if(product) {
+      const deleteProduct = await product.remove();
+      res.send({ message: 'Product Deleted', product: deleteProduct });
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  })
+);
 
 exports.productRouter = productRouter;
