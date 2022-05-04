ca15610086dcff19c85b0bd1938f8ac685755901
Suryansh Yadav <suryansh@advanced-infrastructure.co.uk>
2022-05-04 11:54:37 +0530

    fixed issue: buffer timeout in products.find()

--- server.js
+++ server.js
@@ -1,44 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const dotenv = require('dotenv');
-const { productRouter } = require('./routers/productRouter.js');
-const { userRouter } = require('./routers/userRouter.js');
-const { orderRouter } = require('./routers/orderRouter.js');
-const { uploadRouter } = require('./routers/uploadRouter.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { productRouter } = require("./routers/productRouter.js");
+const { userRouter } = require("./routers/userRouter.js");
+const { orderRouter } = require("./routers/orderRouter.js");
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shopify', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.use('/api/uploads', uploadRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});
-
-const _dirname = path.resolve();
-app.use('/uploads', express.static(path.join(_dirname, '/uploads')));
-
-app.get('/', (req, res) => {
-    res.send("Server is working");
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "Shopify Server Online!",
+  });
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+  console.log(err);
+  res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-    console.log(`Serving at http://localhost:${port}`);
-});
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URL || "mongodb://localhost/shopify", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`Server Online!!`);
+    })
+  )
+  .catch((err) => console.log(err.message));
